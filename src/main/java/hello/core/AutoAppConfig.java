27186785df8844e7f02b6dd006bcd2cc1d685935
TaeHye0n <thk001229@gmail.com>
2023-07-04 20:49:20 +0900

    explain Same name Bean Conflict

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    //자동 빈등록 vs 자동 빈등록: ConflictingBeanDefinitionException 예외 발생
+    //수동 빈등록 vs 자동 빈등록: 수동 빈이 자동 빈을 오버라이딩 해버린다. 하지만 스프링부트에선 오류가 발생나도록 처리함
     @Bean(name = "memoryMemberRepository")
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
