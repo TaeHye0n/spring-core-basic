10ffb41f69508a4ac8a03b141acbd05c885eec51
TaeHye0n <thk001229@gmail.com>
2023-07-05 20:47:00 +0900

    add Dependency Auto Injection with Autowire

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,10 @@ public class AutoAppConfig {
 
     //자동 빈등록 vs 자동 빈등록: ConflictingBeanDefinitionException 예외 발생
     //수동 빈등록 vs 자동 빈등록: 수동 빈이 자동 빈을 오버라이딩 해버린다. 하지만 스프링부트에선 오류가 발생나도록 처리함
+/*
     @Bean(name = "memoryMemberRepository")
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+*/
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,22 +1,43 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //@Autowired 생성자가 딱 1개인 경우 안써도 자동으로 적용
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//    @Autowired(required = false)
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
